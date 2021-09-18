ece82681e07a183e8f7086b501227a4da721b958
Max <you@example.com>
2021-09-18 20:43:35 +1000

    feature: use-fetch hook

---
+++ src/hooks/use-fetch.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+interface IRequestConfig {
+  method?: 'get' | 'post' | 'put' | 'delete';
+  body?: Record<string, any>;
+  headers?: Record<string, string>;
+};
+
+type UseFetchReturnValues = [response: any, makeRequest: () => void];
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+export const useFetch = (url: string, config: IRequestConfig = {}): UseFetchReturnValues => {
+  const { method = 'get', body = null, headers = null } = config;
+
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
+
+  const axiosConfig: AxiosRequestConfig = {
+    method,
+    url,
+    cancelToken: source.token,
+  };
+
+  if (body) axiosConfig.data = body;
+  if (headers) axiosConfig.headers = headers;
+
+  const [response, setResponse] = useState<AxiosResponse>();
+
+  useEffect(() => {
+    return () => {
+      source.cancel('Request has been canceled!');
+    }
+  }, [source]);
+
+  const makeRequest = () => {
+    axios(axiosConfig)
+      .then(setResponse)
+      .catch(console.error);
+  };
+
+  return [response, makeRequest];
+};
